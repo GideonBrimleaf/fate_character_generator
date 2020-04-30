5fd4993a442eebd5308f145a21363fbf4832740a
GideonBrimleaf <chrisdryden1@gmail.com>
2020-04-30 17:26:13 +0100

    form started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import CharacterList from './containers/CharacterList'
+import CharacterForm from './containers/CharacterForm'
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to the Fate Accelerated Character Manager!</h1>
       <CharacterList />
+      <CharacterForm />
     </div>
   );
 }
---
+++ src/containers/CharacterForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CharacterForm extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="This is going to be my awesome form!" />
+      </form>
+    )
+  }
+}
