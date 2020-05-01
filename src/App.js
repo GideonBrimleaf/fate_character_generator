3275ec2897b0cca7f18610d6fb6538dc5865d708
GideonBrimleaf <chrisdryden1@gmail.com>
2020-05-01 18:02:08 +0100

    can successfully route to individual pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import CharacterList from './containers/CharacterList'
 import CharacterForm from './containers/CharacterForm'
 import NavBar from './components/NavBar'
+import CharacterDetail from './components/CharacterDetail'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 class App extends Component {
@@ -12,7 +13,7 @@ class App extends Component {
     this.state = {
       characters:[
                   {id: 1, name:"Gideon Brimleaf", highConcept:"Tiefling Black Metal Bard", trouble:"Doomed to be cursed by dark forces"},
-                  {id:2, name:"Tala Fernweaver", highConcept:"Draws on the powers of gods and demons", trouble:"Doesn't know when to quit"}
+                  {id: 2, name:"Tala Fernweaver", highConcept:"Draws on the powers of gods and demons", trouble:"Doesn't know when to quit"}
                 ]
     }
     this.addCharacter = this.addCharacter.bind(this)
@@ -25,17 +26,22 @@ class App extends Component {
 
   render(){
 
-  return (
-    <div className="App">
-      <Router>
-        <NavBar />
-        <Switch>
-        <Route exact path="/" render={() => <CharacterList characters={this.state.characters} />} />
-        <Route path="/new" render={() => <CharacterForm onCharacterCreated={this.addCharacter} />} />
-        </Switch>
-      </Router>
-    </div>
-  )
+    const characterPaths = this.state.characters.map(character =>{
+      return <Route key={character.id} path={`/character/${character.id}`} render={() => <CharacterDetail character={character} />}/>
+    })
+
+    return (
+      <div className="App">
+        <Router>
+          <NavBar />
+          <Switch>
+          <Route exact path="/" render={() => <CharacterList characters={this.state.characters} />} />
+          <Route path="/new" render={() => <CharacterForm onCharacterCreated={this.addCharacter} />} />
+          {characterPaths}
+          </Switch>
+        </Router>
+      </div>
+    )
   }
 }
 
--- src/components/CharacterSummary.js
+++ src/components/CharacterSummary.js
@@ -3,11 +3,13 @@ import './CharacterSummary.css'
 
 const CharacterSummary = (props) => {
   return (
-    <li className="characterSummaryTile">
-      <h3>{props.characterDetails.name}</h3>
-      <p>{props.characterDetails.highConcept}</p>
-      <p>{props.characterDetails.trouble}</p>
-    </li>
+    <a href={`/character/${props.characterDetails.id}`}>
+      <li className="characterSummaryTile">
+        <h3>{props.characterDetails.name}</h3>
+        <p>{props.characterDetails.highConcept}</p>
+        <p>{props.characterDetails.trouble}</p>
+      </li>
+    </a>
   )
 }
 
