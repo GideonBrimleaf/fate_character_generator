eeb50e23c98546c6638d12bd2286e95de359bd39
GideonBrimleaf <chrisdryden1@gmail.com>
2020-07-10 09:46:39 +0100

    adds auth to firebase file

--- src/fire.js
+++ src/fire.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
+import 'firebase/auth'
 
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,3 +13,4 @@ firebase.initializeApp({
 });
 
 export const db = firebase.database()
+export const auth = firebase.auth()
