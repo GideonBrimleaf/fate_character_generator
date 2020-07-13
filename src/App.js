de31a989ce11933841eae3cfa3a769c5e7d1e84a
GideonBrimleaf <chrisdryden1@gmail.com>
2020-07-13 14:21:06 +0100

    adds alternate private route to individual character routes

--- src/App.js
+++ src/App.js
@@ -56,7 +56,8 @@ class App extends Component {
             <Switch>
             <PrivateRoute exact path="/" component={CharacterList} data={{ characters:this.state.characters, deleteCharacter:this.deleteCharacter }} />
             <PrivateRoute path="/new" component={CharacterForm} data={{ onCharacterCreated:this.addCharacter }} />
-            <Route path="/character/:characterId" render={(matchProps) => <CharacterDetail {...matchProps} characters={this.state.characters}/>} />
+            <PrivateRoute path="/character/:characterId" component={CharacterDetail} data={{ characters:this.state.characters }} />
+            {/* <Route path="/character/:characterId" render={(matchProps) => <CharacterDetail {...matchProps} characters={this.state.characters}/>} /> */}
             <Route exact path="/login" component={Login} /> 
             </Switch> 
           </Router>
