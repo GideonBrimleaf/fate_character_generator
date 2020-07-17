dee44292f5ff65e363b83d53b5b09dba47489336
GideonBrimleaf <chrisdryden1@gmail.com>
2020-07-17 16:17:11 +0100

    adds character details

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './CharacterDetail.css'
+import CharacterStunt from './CharacterStunt'
 
 const CharacterDetail = (props) => {
 
@@ -7,15 +8,55 @@ const CharacterDetail = (props) => {
     return parseInt(props.match.params.characterId) === character.id
   })
 
+  const characterStunts = foundCharacter.stunts.map(stunt => {
+    return(
+      <CharacterStunt characterStunt={stunt} key={stunt} />
+    )
+  })
+
   return (
     <main className="character-sheet">
-      <h1>{foundCharacter.name}</h1>
+      <h1>{ foundCharacter.name }</h1>
       <section className="character-stats">
-        <article>Refresh</article>
-        <article>Aspects</article>
-        <article>Approaches</article>
-        <article>Stunts</article>
-        <article>Consequences</article>
+        <article>
+          <h5>Refresh</h5>
+          <p>{ foundCharacter.refresh }</p>
+        </article>
+        <article>
+          <h5>Aspects</h5>
+          <ul>
+            <li>{ foundCharacter.highConcept }</li>
+            <li>{ foundCharacter.trouble }</li>
+            <li>{ foundCharacter.relationship }</li>
+            <li>{ foundCharacter.aspectOne }</li>
+            <li>{ foundCharacter.aspectTwo }</li>
+          </ul>
+        </article>
+        <article>
+          <h5>Approaches</h5>
+          <ul>
+            <li>Careful: { foundCharacter.approaches.careful }</li>
+            <li>Clever: { foundCharacter.approaches.clever }</li>
+            <li>Flashy: { foundCharacter.approaches.flashy }</li>
+            <li>Forceful: { foundCharacter.approaches.forceful }</li>
+            <li>Quick: { foundCharacter.approaches.quick }</li>
+            <li>Sneaky: { foundCharacter.approaches.sneaky }</li>
+          </ul>
+        </article>
+        <article>
+          <h5>Stunts</h5>
+          <ul>
+            { characterStunts }
+          </ul>
+        </article>
+        <article>
+          <h5>Consequences</h5>
+          <ul>
+            <li>Mild: { foundCharacter.consequences.mild }</li>
+            <li>Moderate: { foundCharacter.consequences.moderate }</li>
+            <li>Severe: { foundCharacter.consequences.severe }</li>
+          </ul>
+        </article>
       </section>
     </main>
   )
---
+++ src/components/CharacterStunt.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CharacterStunt = (props) => {
+  return <li>{ props.characterStunt }</li>
+}
+
+export default CharacterStunt
