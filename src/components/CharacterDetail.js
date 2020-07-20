dc6fb809dcaf8d616a571332b750bb86690b030b
GideonBrimleaf <chrisdryden1@gmail.com>
2020-07-20 12:33:16 +0100

    can use object keys as labels for stat blocks

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './CharacterDetail.css'
+import './CharacterStatBlock.css'
+import CharacterStatBlock from './CharacterStatBlock'
 
 const CharacterDetail = (props) => {
 
@@ -8,7 +10,9 @@ const CharacterDetail = (props) => {
   })
   
   const characterStunts = foundCharacter.stunts.map(stunt => {
-    return (<li key={stunt}>{ stunt }</li>)
+    return (
+      <li key={stunt}>{ stunt }</li>
+      )
   })
 
   return (
@@ -17,30 +21,17 @@ const CharacterDetail = (props) => {
         <h1 className="character-name character-sheet-item-primary">{ foundCharacter.name }</h1>
         <article className="character-sheet-item-secondary">
           <h5>Refresh</h5>
-          <p>{ foundCharacter.refresh }</p>
+          <ul className="stat-block">
+            <li>{ foundCharacter.refresh }</li>
+          </ul>
         </article>
       </section>
       <section className="character-stats">
         <article className="character-sheet-item-primary">
-          <h5>Aspects</h5>
-          <ul className="stat-block">
-            <li>{ foundCharacter.aspects.highConcept }</li>
-            <li>{ foundCharacter.aspects.trouble }</li>
-            <li>{ foundCharacter.aspects.relationship }</li>
-            <li>{ foundCharacter.aspects.aspectOne }</li>
-            <li>{ foundCharacter.aspects.aspectTwo }</li>
-          </ul>
+          <CharacterStatBlock characterStatCategory="Aspects" characterStats={ foundCharacter.aspects } />
         </article>
         <article className="character-sheet-item-secondary">
-          <h5>Approaches</h5>
-          <ul className="stat-block">
-            <li>Careful: { foundCharacter.approaches.careful }</li>
-            <li>Clever: { foundCharacter.approaches.clever }</li>
-            <li>Flashy: { foundCharacter.approaches.flashy }</li>
-            <li>Forceful: { foundCharacter.approaches.forceful }</li>
-            <li>Quick: { foundCharacter.approaches.quick }</li>
-            <li>Sneaky: { foundCharacter.approaches.sneaky }</li>
-          </ul>
+          <CharacterStatBlock characterStatCategory="Approaches" characterStats={ foundCharacter.approaches } />
         </article>
       </section>
       <section className="character-stats">
@@ -50,15 +41,10 @@ const CharacterDetail = (props) => {
             { characterStunts }
           </ul>
         </article>
-      </section>
+      </section> 
       <section className="character-stats">
         <article className="character-sheet-item-primary">
-          <h5>Consequences</h5>
-          <ul className="stat-block">
-            <li>Mild: { foundCharacter.consequences.mild }</li>
-            <li>Moderate: { foundCharacter.consequences.moderate }</li>
-            <li>Severe: { foundCharacter.consequences.severe }</li>
-          </ul>
+          <CharacterStatBlock characterStatCategory="Consequences" characterStats={ foundCharacter.consequences } />
         </article>
       </section>
     </main>
---
+++ src/components/CharacterStatBlock.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './CharacterStatBlock.css'
+
+const CharacterStatBlock = (props) => {
+
+  const statItemNames = Object.keys(props.characterStats)
+
+  const prettifyName = (objectKey) => {
+    const capitalisedFirstLetter = objectKey.charAt(0).toUpperCase() + objectKey.slice(1)
+    const splitName = capitalisedFirstLetter.split(/(?=[A-Z])/)
+    return splitName.join(' ')
+  }
+
+  const statItems = statItemNames.map(stat => {
+    return (
+      <li>{ prettifyName(stat) }: { props.characterStats[stat] }</li>
+    )
+  })
+
+  return (
+    <>
+      <h5>{ props.characterStatCategory }</h5>
+      <ul className="stat-block">
+        { statItems }
+      </ul>
+    </>
+  )
+}
+
+export default CharacterStatBlock
