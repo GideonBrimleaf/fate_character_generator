e5e2ccf1172e9846969f4c14169e6fb1fd5646ba
GideonBrimleaf <chrisdryden1@gmail.com>
2020-07-22 16:34:46 +0100

    can check stress boxes with local state

--- src/components/StressBox.js
+++ src/components/StressBox.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './StressBox.css'
 
 const StressBox = () => {
+
+  const [checked, setChecked] = useState([false, false, false])
+
+  const checkBox = (event) => {
+    const boxNumber = event.target.innerText
+    if (checked[boxNumber-1]) {
+      const updatedCheckedBoxes = [...checked]
+      updatedCheckedBoxes[boxNumber-1] = false
+      setChecked(updatedCheckedBoxes)
+    } else {
+      const updatedCheckedBoxes = [...checked]
+      updatedCheckedBoxes[boxNumber-1] = true
+      setChecked(updatedCheckedBoxes)
+    }
+  }
+
   return (
     <table>
       <caption>Stress</caption>
       <tbody>
         <tr>
-          <td className="stress-box">1</td>
-          <td className="stress-box">2</td>
-          <td className="stress-box">3</td>
+          <td onClick={checkBox} className={`stress-box ${checked[0] ? "checked-box" : ""}`}>1</td>
+          <td onClick={checkBox} className={`stress-box ${checked[1] ? "checked-box" : ""}`}>2</td>
+          <td onClick={checkBox} className={`stress-box ${checked[2] ? "checked-box" : ""}`}>3</td>
         </tr>
       </tbody>
     </table>
