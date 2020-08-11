6d62d9b34527af96a3d27bcbd849d114a1e88ee0
GideonBrimleaf <chrisdryden1@gmail.com>
2020-08-11 16:48:05 +0100

    stat block added to summary

--- src/components/CharacterSummary.js
+++ src/components/CharacterSummary.js
@@ -11,15 +11,31 @@ const CharacterSummary = (props) => {
     }
   }
 
+  const characterApproaches = Object.keys(props.characterDetails.approaches)
+
+  const skillsHeaders = characterApproaches.map(skill => {
+    const abbreviatedSkill = skill.substring(0,2)
+    return <td>{abbreviatedSkill.charAt(0).toUpperCase() + abbreviatedSkill.slice(1)}</td>
+  })
+
+  const skills = characterApproaches.map(skill => {
+    return <td>{props.characterDetails.approaches[skill]}</td>
+  })
+
   return (
     <li className="characterSummaryTile">
-      <section>
-        <Link className="character-link" to={`/character/${props.characterDetails.id}`}>
+      <Link className="character-link" to={`/character/${props.characterDetails.id}`}>
+        <div className="character-summary-details">
           <h3>{props.characterDetails.name}</h3>
           <p>{props.characterDetails.aspects.highConcept}</p>
           <p>{props.characterDetails.aspects.trouble}</p>
-        </Link>
-      </section>
+        </div>
+        <table className="character-skills-table">
+          <caption>Approaches</caption>
+          <tr>{ skillsHeaders }</tr>
+          <tr>{ skills }</tr>
+        </table>
+      </Link>
       <section className="option-links">
         <button className="primary-button" onClick={handleCharacterDeletion}>Delete</button>
         <Link className="primary-button" to={`/character/${props.characterDetails.id}/edit`}>Edit</Link>
