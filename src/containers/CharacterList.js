e80ae4f81154d09282bf968a51f97e65260003cb
GideonBrimleaf <chrisdryden1@gmail.com>
2021-03-11 17:48:20 +0000

    line lengths fixed

--- src/containers/CharacterList.js
+++ src/containers/CharacterList.js
@@ -1,32 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import CharacterSummary from '../components/CharacterSummary'
 import './CharacterList.css'
 import '../App.css'
-import { Link } from 'react-router-dom'
 
 const CharacterList = (props) => {
-  const characterList = props.characters ? props.characters.map(character =>{
-    return (
-      <CharacterSummary characterDetails={character} key={character.id} deleteCharacter={props.deleteCharacter}/>
-    )
-  }) : null
+  const characterList = props.characters ? props.characters.map((character) => (
+    <CharacterSummary
+      characterDetails={character}
+      key={character.id}
+      deleteCharacter={props.deleteCharacter}
+    />
+  )) : null
 
   const newCharacterButton = <Link className="primary-button" to="/new">Create a New Character</Link>
 
-  const optionalCharacterButton = (
-    props.characters.length > 0 ? newCharacterButton : null
-  )
+  const optionalCharacterButton = (
+    props.characters.length > 0 ? newCharacterButton : null
+  )
 
-  return (
-    <main>
-      <h1>Fate Character Manager</h1>
+  return (
+    <main>
+      <h1>Fate Character Manager</h1>
       {newCharacterButton}
-      <ul className="characterList">
-        {characterList}
-      </ul>
-        {optionalCharacterButton}
-    </main>
-  )
+      <ul className="characterList">
+        {characterList}
+      </ul>
+      {optionalCharacterButton}
+    </main>
+  )
 }
 
 export default CharacterList
