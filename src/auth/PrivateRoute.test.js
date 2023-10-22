79b308836a0619a0425d3ce0203fafa0017755be
Jack Schofield <jjmschofield@googlemail.com>
2023-10-22 21:33:54 +0100

    chore: adding tests for auth components

---
+++ src/auth/PrivateRoute.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
+
+import { AuthContext } from './Auth';
+import PrivateRoute from './PrivateRoute';
+
+it('renders nothing when the user is not logged in', () => {
+  const result = render(
+    <AuthContext.Provider value={{ currentUser: null }}>
+      <MemoryRouter>
+        <PrivateRoute />
+      </MemoryRouter>
+    </AuthContext.Provider>
+  );
+
+  expect(result.asFragment()).toMatchSnapshot();
+});
+
+
+it('renders children when the user is not set', () => {
+  const result = render(
+    <AuthContext.Provider value={{ currentUser: true }}>
+      <MemoryRouter>
+        <PrivateRoute component={() => <div id="expected" />}/>
+      </MemoryRouter>
+    </AuthContext.Provider>
+  );
+
+  expect(result.asFragment()).toMatchSnapshot();
+});
