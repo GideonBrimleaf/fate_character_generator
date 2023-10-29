e6a8b55149afc554a7d42abc098cb5a6c0368a80
Jack Schofield <jjmschofield@googlemail.com>
2023-10-29 22:10:21 +0000

    chore: converting fire.js to fire.ts

--- src/lib/models/Character.ts
+++ src/lib/models/Character.ts
@@ -24,3 +24,39 @@ export interface Character {
   },
   refresh: number,
 }
+
+// interface ICharacterStore {}
+
+// export class CharacterStore implements ICharacterStore {
+//     devURL: string = 'http://localhost:8080/characters';
+//     db
+
+//     constructor(){
+//         const rootRef = db.ref('characters');
+//     }
+
+//     updateCharacters(charactersToStore: Character[]): Promise<any> {
+//         localStorage.setItem('characters', JSON.stringify(charactersToStore));
+    
+//         if (process.env.NODE_ENV !== 'production') {
+//           return fetch(devURL, {
+//             method: 'POST',
+//             body: JSON.stringify(charactersToStore),
+//             headers: { 'Content-Type': 'application/json' },
+//           })
+//             .then((res) => res.json())
+//             .catch((error) => console.log(error));
+//         }
+//         return rootRef.set(charactersToStore);
+//       }
+    
+//       getCharacters(): Promise<Character[] | undefined> {
+//         if (process.env.NODE_ENV !== 'production') {
+//           return fetch(devURL)
+//             .then((res) => res.json())
+//             .then((data) => data.characters);
+//         }
+    
+//         return rootRef.once('value').then((data) => data.val());
+//       },
+// }
